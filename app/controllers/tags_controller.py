056c7c2177aac89c4852ef03c8a9d28edb4c8a6d
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-02-02 09:55:43 +0000

    complete tag management front end

--- app/controllers/tags_controller.py
+++ app/controllers/tags_controller.py
@@ -7,3 +7,31 @@ import app.repositories.transaction_repository as transaction_repository
 from app.models.user_assets import current_user
 
 tags_blueprint = Blueprint("tags", __name__)
+
+@tags_blueprint.route("/tags")
+def tags():
+
+    tags = tag_repository.select_all()
+    colours = colour_repository.select_all()
+
+    return render_template("/tags/index.html", tags = tags, colours = colours)
+
+
+@tags_blueprint.route("/tags/<id>/edit", methods=['post'])
+def edit_tag(id):
+
+    tag = tag_repository.select(int(id))
+    colour = colour_repository.select(int(request.form['colour_choice']))
+
+
+    if not tag.reserved:
+
+        if request.form.get('is_active') == 'is_active':
+            tag.activate()
+        else:
+            tag.deactivate()
+    
+    tag.change_colour(colour)
+    tag_repository.update(tag)
+
+    return redirect(request.referrer)
